d108a4d144625b303dcc61c13e07727df4ac9350
poeMoon0416 <siz0416z@gmail.com>
2025-08-11 22:40:09 +0900

    コンポーネント分割

---
+++ demo-front/src/components/FindSaleView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>FindSaleView</h2>
+  <v-number-input v-model="saleViewId" label="SaleView Id:" />
+  <v-btn @click="findSaleView">検索</v-btn>
+  <pre>
+    <code>
+      {{ saleView }}
+    </code>
+  </pre>
+</template>
+
+<script setup lang="ts">
+// imports and useStore()
+import { useShopsStore } from "@/stores/shops";
+import type { SaleView } from "@/types/shops/Sale";
+const store = useShopsStore();
+
+// SaleView
+const saleViewId = ref<number>(-1);
+
+const findSaleView = async () => {
+  store.findSaleView(saleViewId.value);
+};
+
+const saleView = computed<SaleView>(() => store.saleView);
+</script>
